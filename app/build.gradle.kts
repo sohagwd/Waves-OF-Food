4a889755e137902bbbd47cc83166d835c5c2c45b
sohagwd <sohag.wd.bd@gmail.com>
2023-10-23 13:30:03 +0600

    admin login form

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,13 +4,16 @@ plugins {
 }
 
 android {
+    buildFeatures{
+        viewBinding = true
+    }
     namespace = "com.fery.adminwaveoffood"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.fery.adminwaveoffood"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
